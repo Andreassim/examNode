86e6bf8d90ab26345b147dc8de2443adaa67f1a8
Andreas Simmelkiaer <andreas.sim@live.dk>
2023-05-03 12:51:28 +0200

    initial client and logo

---
+++ client/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {Router, Link, Route} from "svelte-navigator";
+  import Home from "./pages/home/Home.svelte";
+  import Profile from "./pages/profile/Profile.svelte";
+</script>
+
+<main>
+  <Router>
+    <nav>
+    <img src="/icon.svg" alt="">
+    <h1>API Callback Reciever</h1>
+    <Link to="/">Home</Link>
+    <Link to="/profile">Profile</Link>
+  </nav>
+  
+  <div>
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="/profile">
+      <Profile />
+    </Route>
+  </div>
+</Router>
+</main>
