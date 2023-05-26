7e0035090ed813c6613930c88a5b65009af19eb0
Andreas Simmelkiaer <andreas.sim@live.dk>
2023-05-26 11:45:05 +0200

    initial sessionlist

---
+++ client/src/components/sessionList/SessionList.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Button } from "flowbite-svelte";
+    import { session } from "../../store/sessionStore/sessionStore";
+    import { useNavigate } from "svelte-navigator";
+
+    export let sessions;
+
+    const navigate = useNavigate();
+
+    function handleNagigateToSession(sessionId){
+        $session.id = sessionId;
+        navigate(`/$/${$session.id}`);
+    }
+
+</script>
+
+<div class="m-2 border rounded-lg text-left">
+    <ul>
+        {#each Object.values(sessions) as session, i}
+        <li class="w-full p-2 border-b-2 {i % 2 == 0 ? "bg-gray-100" : ""}">
+            <div class="flex items-stretch">
+                <p class="flex-grow mt-2">
+                    {session.id}
+                </p>
+                {#if session.private}
+                    <div class="m-2 px-2 border-2 bg-gray-500 text-white rounded-lg">Private</div>
+                {/if}
+                <Button color="green" outline on:click={() => handleNagigateToSession(session.id)}>Connect</Button>
+            </div>
+        </li>
+        {/each}
+    </ul>
+</div>
--- client/src/pages/profile/Profile.svelte
+++ client/src/pages/profile/Profile.svelte
@@ -1,9 +1,20 @@
 <script>
     import { Card } from "flowbite-svelte";
+    import SessionList from "../../components/sessionList/SessionList.svelte";
+
+    const sessions = {
+        0: { id: 1234},
+        1: { id: 2234},
+        2: { id: 3234},
+        3: { id: 4234, private: true},
+        4: { id: 5234},
+    };
+
 </script>
 
 <div class="grid grid-cols-2 h-full bg-white">
     <div class="col-span-1 border-r-2">
+        <SessionList sessions = {sessions}/>
     </div>
     <div class="col-span-1 mx-auto mt-5">
         <Card size="lg">
